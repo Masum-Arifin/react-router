039be6703f12dd86274fae6797173bd007b49a55
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-03 14:52:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Route, Routes } from "react-router-dom";
+// import "./App.css";
+// import About from "./components/About/About";
+// import Friends from "./components/Friends/Friends";
+// import Home from "./components/Home/Home";
+// import NotFound from "./components/NotFound/NotFound";
+// import Header from "./components/Header/Header";
+// import FriendDetail from "./components/FriendDetail/FriendDetail";
+// import Posts from "./components/Posts/Posts";
+// import PostDetail from "./components/PostDetail/PostDetail";
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header></Header>
+//       <Routes>
+//         <Route path="/" element={<Home />}></Route>
+//         <Route path="/friends" element={<Friends />}></Route>
+//         <Route path="/friend/:friendId" element={<FriendDetail />}></Route>
+//         <Route path="/posts" element={<Posts />}></Route>
+//         <Route path=':postId' element={<PostDetail></PostDetail>}></Route>
+//         <Route path="/about" element={<About />}></Route>
+//         <Route path="*" element={<NotFound />}></Route>
+//       </Routes>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// export default App;
---
+++ src/components/FriendDetail/FriendDetail.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState} from 'react';
+
+const FriendDetail = () => {
+    const {friendId}= useParams();
+    const [friend, setFriend] = useState({})
+    useEffect(()=>{
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setFriend(data))
+    },[])
+    return (
+        <div>
+            <h2>This is Detail of a friend: {friendId}</h2>
+            <h3>Name: {friend.name}</h3>
+            <h4>Email: {friend.email}</h4>
+            <h5>Website:{friend.website}</h5>
+            <p>City: {friend.address?.city}</p>
+            <p>Lat: {friend.address?.geo.lat}</p>
+        </div>
+        
+    );
+};
+
+export default FriendDetail;
