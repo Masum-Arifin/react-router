6ab0c22f282d13cf0419d074262713dcaf98e872
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-03 14:58:35 +0600

    new

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 
-const PostDetail = () => {
-  const { postId } = useParams();
-  const [post, setPost] = useState({})
-  useEffect(() =>{
-      const url = `https://jsonplaceholder.typicode.com/posts/${postId}`
-      fetch(url)
-      .then(res => res.json())
-      .then(data => setPost(data))
-  },[postId])
-  return (
-    <div>
-      <h3>This is post Detail for: {postId}</h3>
-      <h4>{post.title}</h4>
-      <h5>{post.body}</h5>
-    </div>
-  );
-};
+// const PostDetail = () => {
+//   const { postId } = useParams();
+//   const [post, setPost] = useState({})
+//   useEffect(() =>{
+//       const url = `https://jsonplaceholder.typicode.com/posts/${postId}`
+//       fetch(url)
+//       .then(res => res.json())
+//       .then(data => setPost(data))
+//   },[postId])
+//   return (
+//     <div>
+//       <h3>This is post Detail for: {postId}</h3>
+//       <h4>{post.title}</h4>
+//       <h5>{post.body}</h5>
+//     </div>
+//   );
+// };
 
-export default PostDetail;
+// export default PostDetail;
