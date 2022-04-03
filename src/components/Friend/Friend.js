4e471e9d948d65227dcc55644c97ef3a06ee04fc
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-03 14:55:54 +0600

    new

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,21 +1,21 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-// const Friend = (props) => {
-//   const { name, username, id } = props.friend;
-//   const navigate = useNavigate();
-//   const showFriendDetail = () => {
-//     const path = `/friend/${id}`;
-//     navigate(path);
-//   };
-//   return (
-//     <div>
-//       <h2>Name:{name}</h2>
-//       <button onClick={showFriendDetail}>
-//         {username}: {id}
-//       </button>
-//     </div>
-//   );
-// };
+const Friend = (props) => {
+  const { name, username, id } = props.friend;
+  const navigate = useNavigate();
+  const showFriendDetail = () => {
+    const path = `/friend/${id}`;
+    navigate(path);
+  };
+  return (
+    <div>
+      <h2>Name:{name}</h2>
+      <button onClick={showFriendDetail}>
+        {username}: {id}
+      </button>
+    </div>
+  );
+};
 
-// export default Friend;
+export default Friend;
