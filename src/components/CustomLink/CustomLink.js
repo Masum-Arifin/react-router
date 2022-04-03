b9a27ba8edd2e9c3e0ab8c19384fccfc258eeeca
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-03 14:54:54 +0600

    new

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+// import React from 'react';
+// import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
-function CustomLink({ children, to, ...props }) {
-    let resolved = useResolvedPath(to);
-    let match = useMatch({ path: resolved.pathname, end: true });
+// function CustomLink({ children, to, ...props }) {
+//     let resolved = useResolvedPath(to);
+//     let match = useMatch({ path: resolved.pathname, end: true });
   
-    return (
-      <div>
-        <Link
-          style={{ color: match ? 'green': 'red', textDecoration: match ? "underline" : "none" }}
-          to={to}
-          {...props}
-        >
-          {children}
-        </Link>
+//     return (
+//       <div>
+//         <Link
+//           style={{ color: match ? 'green': 'red', textDecoration: match ? "underline" : "none" }}
+//           to={to}
+//           {...props}
+//         >
+//           {children}
+//         </Link>
         
-      </div>
-    );
-  }
+//       </div>
+//     );
+//   }
 
-export default CustomLink;
+// export default CustomLink;
